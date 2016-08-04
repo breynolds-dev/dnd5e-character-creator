f6f62a15939e78d86343b8eb0d4daf86c0d82fc5
arturusfury <bri.reynolds@gmail.com>
2016-08-03 20:47:49 -0500

    started writing a bit of a character model spec

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -3,3 +3,37 @@ require 'rails_helper'
 RSpec.describe Character, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe 'Character' do
+  it 'has a character name and a player name' do
+
+  end
+
+  it 'has a race and subrace if applicable' do
+
+  end
+
+  it 'has a height, weight, age and sex' do
+
+  end
+
+  it 'has an alignment' do
+
+  end
+
+  it 'has a class and class archetype' do
+
+  end
+
+  it 'has all of the attributes (STR, DEX, WIS, CON, INT, CHA)' do
+
+  end
+
+  it 'has a background' do
+
+  end
+
+  it 'has a collection of skills' do
+    
+  end
+end
