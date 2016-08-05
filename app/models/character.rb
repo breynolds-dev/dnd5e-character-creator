8299c2eaad2e38a9f04f16b6b71c60193b95f133
arturusfury <bri.reynolds@gmail.com>
2016-08-05 10:55:15 -0500

    cleaned up extra white space

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,9 @@
+# Character Model
 class Character < ActiveRecord::Base
   validates :name,
             :race,
             :char_class,
-            :char_class_archetype, #determined by class?
+            :char_class_archetype, # determined by class?
             :alignment,
             :strength,
             :dexterity,
@@ -11,5 +12,4 @@ class Character < ActiveRecord::Base
             :charisma,
             :constitution,
             :experience, presence: true
-
 end
